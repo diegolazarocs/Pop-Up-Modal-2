6996285fdf70e1de88ecd514ea0d9571fee871b0
Diego Lázaro <111025421+diegolazarocs@users.noreply.github.com>
2024-06-15 23:12:59 -0300

    Add files via upload

---
+++ Pop Up Modal 2/main.js
@@ -0,0 +1,7 @@
+const modal = document.querySelector(".modal-conteiner");
+function openModal() {
+  modal.classList.add("active");
+}
+function closeModal() {
+  modal.classList.remove("active");
+}
